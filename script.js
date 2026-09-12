1d934976fd8454667087a380883a629bd1f3ade2
Amer <203231960+Anthrax-2@users.noreply.github.com>
2026-09-12 15:12:03 +0300

    Create function 'createCanvas' that initializes the canvas

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 "use strict"
 
+const body = document.querySelector("body")
+const canvasContainer = document.createElement("div")
+canvasContainer.classList.add("canvas-container")
+body.appendChild(canvasContainer)
+
+function createCanvas(side = 16) {
+    if (side > 100 || side <= 0) {
+        throw new Error("Input must be less than 100 and more than 0")
+    }
+
+    if (canvasContainer.hasChildNodes()) {
+        canvasContainer.removeChild(canvasContainer.firstChild)
+    }
+
+    const canvas = document.createElement("div")
+    canvas.classList.add("canvas")
+
+    canvas.style.height = "550px"
+    canvas.style.width = "550px"
+
+    canvasContainer.appendChild(canvas)
+
+    const pSide = parseInt(canvas.style.width) / side
+
+    for (let i = 0; i < side * side; i++) {
+        const pixel = document.createElement("div")
+        pixel.style.backgroundColor = "white"
+        pixel.style.height = `${pSide}px`
+        pixel.style.width = `${pSide}px`
+
+        canvas.appendChild(pixel)
+    }
+    
+}
+
+createCanvas(64)
 
